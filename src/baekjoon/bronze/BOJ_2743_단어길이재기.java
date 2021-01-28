5a663c684776b67cfcfa4957beaaffe243015932
LEEAYOUNG <souk0711@gmail.com>
2021-01-28 23:47:05 +0900

    [Add]Baekjoon 2743 : 문자열

---
+++ src/baekjoon/bronze/BOJ_2743_단어길이재기.java
@@ -0,0 +1,12 @@
+package baekjoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+//백준_단어길이재기_2743_브론즈2
+public class BOJ_2743_단어길이재기 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(br.readLine().length());
+	}
+}
