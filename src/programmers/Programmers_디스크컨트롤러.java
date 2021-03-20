bde6986b858c12bf0a1cd2d5879cab1915ca59d9
LEEAYOUNG <souk0711@gmail.com>
2021-03-20 21:50:32 +0900

    [Add] Programmers : heap

---
+++ src/programmers/Programmers_디스크컨트롤러.java
@@ -0,0 +1,14 @@
+package programmers;
+
+//Programmers_디스크컨트롤러_level3
+public class Programmers_디스크컨트롤러 {
+	public static void main(String[] args) {
+		int[][] jobs = { { 0, 3 }, { 1, 9 }, { 2, 6 } };
+		System.out.println(solution(jobs));
+	}
+
+	public static int solution(int[][] jobs) {
+		int answer = 0;
+		return answer;
+	}
+}
