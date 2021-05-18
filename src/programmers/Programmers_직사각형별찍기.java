8b9cc527d6074fdc1a61bf0a04059e64194fff73
LEEAYOUNG <souk0711@gmail.com>
2021-05-18 23:41:40 +0900

    Programmers : IO

---
+++ src/programmers/Programmers_직사각형별찍기.java
@@ -0,0 +1,26 @@
+package programmers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// Programmers_직사각형별찍기_level1
+public class Programmers_직사각형별찍기 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < b; i++) {
+			for (int j = 0; j < a; j++) {
+					sb.append("*");
+			}
+			sb.append("\n");
+		}
+		
+		System.out.println(sb.toString());
+	}
+}
