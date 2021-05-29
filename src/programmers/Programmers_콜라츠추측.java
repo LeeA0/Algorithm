fbaf037a920a9cc75556f1a8d6a64fdad2c04f76
LEEAYOUNG <souk0711@gmail.com>
2021-05-29 23:59:31 +0900

    Programmers : math

---
+++ src/programmers/Programmers_콜라츠추측.java
@@ -0,0 +1,26 @@
+package programmers;
+
+public class Programmers_콜라츠추측 {
+
+	public static void main(String[] args) {
+		int num = 626331;
+		System.out.println(solution(num));
+	}
+
+	public static int solution(int num) {
+		int answer = 0;
+		long n = num;
+		while (n != 1) {
+			answer++;
+			if (answer > 500) {
+				break;
+			}
+			if (n % 2 == 0) {
+				n /= 2;
+			} else {
+				n = n * 3 + 1;
+			}
+		}
+		return answer > 500 ? -1 : answer;
+	}
+}
