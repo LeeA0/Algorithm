50f4b61092b921c061facee17c0ba28e7a711db1
LeeA0 <lay0711@naver.com>
2021-08-27 10:05:53 +0900

    BOJ : math

---
+++ src/baekjoon/bronze/BOJ_1550_16진수.java
@@ -0,0 +1,12 @@
+package baekjoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+// BOJ_1550_16진수_브론즈5
+public class BOJ_1550_16진수 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(Integer.parseInt(br.readLine(), 16));
+	}
+}
