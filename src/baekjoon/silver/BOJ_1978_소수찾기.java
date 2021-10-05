19975e8cc4e91c9f2a8ab1e023dc6e0cb80a4936
LeeA0 <lay0711@naver.com>
2021-10-05 23:49:48 +0900

    BOJ : math

---
+++ src/baekjoon/silver/BOJ_1978_소수찾기.java
@@ -0,0 +1,33 @@
+package baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// BOJ_1978_소수찾기_실버4
+public class BOJ_1978_소수찾기 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int count = 0;
+
+		StringTokenizer st = new StringTokenizer(br.readLine().trim(), " ");
+		for (int i = 0; i < N; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			if (num != 1 && isPrime(num)) {
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+
+	public static boolean isPrime(int num) {
+		int square = (int) Math.sqrt(num);
+		for (int i = 2; i <= square; i++) {
+			if (num % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
