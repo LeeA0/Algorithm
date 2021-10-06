b3fac5a05ede411d06383445fd0b66f728fcddbf
LeeA0 <lay0711@naver.com>
2021-10-06 22:42:39 +0900

    BOJ : queue

---
+++ src/baekjoon/silver/BOJ_2164_카드2.java
@@ -0,0 +1,22 @@
+package baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BOJ_2164_카드2 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		Queue<Integer> cards = new LinkedList<>();
+		for (int i = 1; i <= N; i++) {
+			cards.offer(i);
+		}
+		while(cards.size()!=1) {
+			cards.poll();
+			cards.offer(cards.poll());
+		}
+		System.out.println(cards.peek());
+	}
+}
